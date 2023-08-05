0f26c3ed6113518d45791ce6095a9059eb0a0f84
kapishupadhyay22 <92024461+kapishupadhyay22@users.noreply.github.com>
2023-08-06 01:06:18 +0530

    commit for shaanti

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
+const tasks  = require('./routes/tasks')
 
-//routes
+// middleware
+
+app.use(express.json())
+
+// routes
+
 app.get('/hello' , (req, res) =>{
-    res.send('Task manager app')
+    res.send(' Task manager app')
 })
 
+app.use('/api/v1/tasks' , tasks); 
 
+// app.get('api/v1/tasks')          get all the tasks
+// app.post('api/v1/tasks')         create the task
+// app.get('api/v1/tasks/:id')      get single task
+// app.patch('api/v1/tasks/:id')    update task
+// app.delete('api/v1/tasks/:id')   delete task
 
 const port = 3000
 
